97b1f4d85506029ae49d105fb71593fdcd31580d
pearpearB <smilyoung@gmail.com>
2022-06-01 14:25:23 +0900

    [Feat] 페이지 만들기 #2

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,3 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import './Main.scss';
+
 export default function Main() {
-  return <>???</>;
+  const navigate = useNavigate();
+
+  const navigateHandler = (menu: string) => {
+    navigate(`/${menu}`);
+  };
+
+  return (
+    <>
+      <div className='MainWrap'>
+        <div className='MainName'>Main</div>
+        <div className='ButtonWrap'>
+          <button onClick={() => navigateHandler('ranking')}>Ranking</button>
+          <button onClick={() => navigateHandler('tweet')}>Tweet</button>
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/pages/Ranking/Ranking.tsx
+++ client/src/pages/Ranking/Ranking.tsx
@@ -1,3 +1,3 @@
 export default function Ranking() {
-  return <></>;
+  return <>Ranking</>;
 }
